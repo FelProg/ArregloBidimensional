9fee216801213a2ea138799b650a4690040e5181
German Felix Moroyoqui <f.moroyoqui@gmail.com>
2020-07-22 11:48:32 -0700

    Menú de opciones

--- ArregloBidimensional/Program.cs
+++ ArregloBidimensional/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Linq;
 
 namespace ArregloBidimensional
@@ -11,7 +12,7 @@ namespace ArregloBidimensional
     
     class Program
     {
-
+        //datos de los alumnos inicializados.
         static string[,] califAlumnos = new string[,]
             {
                 {"Abel    ","10","9","10","10" },
@@ -21,21 +22,63 @@ namespace ArregloBidimensional
                 {"José    ","10","10","10","10" }
             };
         
-        //arreglo para tomar los valores de las calificaciones numericas
+        //arreglo para tomar los valores de las calificaciones numericas.
         static decimal[] total = new decimal[4];
 
         static void Main(string[] args)
         {
-            DespliegaTabla();
-            DespliegaRepoIndividual();
+            MenuPrincipal();
+            Console.ReadLine();
+        }
+        private static void MenuPrincipal()
+        {
+            Console.Clear();
+            Console.Write($"\n\tBienvenido al programa de contról de calificaciones" +
+                $"\n\tSeleccione el número de la opcion deseada  " +
+                $"\n\n\t1.  Despliegue de calificaciones totales" +
+                $"\n\t2.  Consulta de alumno" +
+                $"\n\t3.  Salir   " +
+            $"\n\n\tDigite la opción :   ");
+            try
+            {
+                int op = Int32.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        DespliegaTabla();
+                        break;
+                    case 2:
+                        DespliegaRepoIndividual();
+                        break;
+                    case 3:
+                        Console.WriteLine("\n\n\tGracias!!! Que pase un buen día.");
+                        break;
+                    default:
+                        Console.Clear();
+                        throw new Exception ("\nOpcion invalida");
+                }
+               
+            }
+            catch(Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("\n\tOpcion invalida del tipo: "+ex.Message);
+                Console.ReadLine();
+                MenuPrincipal();
 
+            }
             
         }
+
         private static void Encabezado(bool principal = true, string nombre ="anonymus")
         {
+            Console.Clear();
             if(principal)
+            //Despliega si principal es true (valor de default)
             Console.WriteLine("\n\t\t\t\tRegistro de calificaciones ciclo escolar 2020-2021\n");
             else
+            //Despliega si el método DespliegaReporteIndividual cambia los parámetros al 
+            //llamar a encabezado.
             Console.WriteLine($"\n\t\t\t\tReporte individual del alumno(a) : {nombre}");
 
             Console.WriteLine("\n\tNombre del alumno\tEspañol \tMatemáticas \tProgramación \tProyectos \tPromedio\n");
@@ -49,7 +92,7 @@ namespace ArregloBidimensional
                 for (int j = 0; j < califAlumnos.GetLength(1); j++)
                 {
                     Console.Write($"\t{califAlumnos[i, j]}          ");
-                    //Como almaceno los numeros german
+                    //Almacenamiento de las calificaciones en total.
                     if (j > 0)
                         total[j-1] = Int32.Parse(califAlumnos[i, j]);
                 }
@@ -58,6 +101,7 @@ namespace ArregloBidimensional
             }
             Console.Write("\t");
             Console.ReadLine();
+            MenuPrincipal();
         }
 
         private static void DespliegaRepoIndividual()
@@ -104,6 +148,7 @@ namespace ArregloBidimensional
             
             //***** fin de despliegue de calificaciones individuales ******
             Console.ReadLine();
+            MenuPrincipal();
         }
 
         private static int Encontrar(string nombre)
