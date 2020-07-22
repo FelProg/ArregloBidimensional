61e5091d6d57c80853eaf7447016c39893e13c03
German Felix Moroyoqui <f.moroyoqui@gmail.com>
2020-07-21 20:44:55 -0700

    despliega tabla

--- ArregloBidimensional/Program.cs
+++ ArregloBidimensional/Program.cs
@@ -10,9 +10,38 @@ namespace ArregloBidimensional
     
     class Program
     {
+
+        static string[,] califAlumnos = new string[,]
+            {
+                {"Abel    ","10","9","10","10" },
+                {"Ana     ","10","10","10","9"},
+                {"David   ","8","10","10","8" },
+                {"Germán  ","10","9","10","10" },
+                {"José    ","10","10","10","10" }
+            };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DespliegaTabla();
+
+            
+        }
+
+        private static void DespliegaTabla()
+        {
+            //encabezado de tabla
+           
+            Console.WriteLine("\n\tNombre del alumno\tEspañol \tMatemáticas \tProgramación \tProyectos \n");
+            for (int i = 0; i < califAlumnos.GetLength(0); i++)
+            {
+                for (int j = 0; j < califAlumnos.GetLength(1); j++)
+                {
+                    Console.Write($"\t{califAlumnos[i, j]}          ");
+                }
+                Console.WriteLine();
+            }
+            Console.Write("\t");
+            Console.ReadLine();
         }
     }
 }
