9279766cbe0a76bef7937a49cda64f5ebc9e096a
German Felix Moroyoqui <f.moroyoqui@gmail.com>
2020-07-21 19:33:22 -0700

    Agregar archivos de proyecto.

---
+++ ArregloBidimensional/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArregloBidimensional
+{
+    //Programa que trabaja con 5 materias por alumno, entrega un promedio
+    //el programa solicita el tipo de reporte:
+    //1. Despliegue de la tabla en general
+    //2. Reporte individual con promedio
+    //3. Despliega los 3 primeros lugares
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
